c953e65657291c3db5850913f46625750016cb30
pratiksaxena <pratik2709@gmail.com>
2017-10-31 06:49:33 +0530

    intialized variables

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,4 +1,29 @@
 package Main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = 240;
+    public static final int SCALE = 2;
+
+    //game thread
+    private Thread thread;
+    private boolean running;
+    private int FPS = 60;
+    private long targetTime = 1000/FPS;
+
+    //canvas
+    private BufferedImage image;
+    private Graphics2D g;
+
+    public GamePanel(){
+        super();
+        setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        setFocusable(true);
+        requestFocus();
+    }
+
 }
