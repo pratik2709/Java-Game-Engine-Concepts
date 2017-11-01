20b46925c27b341a0564358292f154d55e9bd3da
pratiksaxena <pratik2709@gmail.com>
2017-11-01 20:59:55 +0530

    intermediate for Game framework

--- src/Main/GameMain.java
+++ src/Main/GameMain.java
@@ -1,6 +1,8 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class GameMain extends JPanel {
     //constants for the game
@@ -22,11 +24,48 @@ public class GameMain extends JPanel {
 
         //UI components
         canvas = new GameCanvas();
-
-
+        canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+        add(canvas);
+        //scorecard, button, etc
+        //setup menu bar
         
     }
 
     private void gameInit() {
+
     }
+
+    private void gameStart(){
+
+    }
+
+    private void gameStop(){
+
+    }
+
+    private void gameUpdate(){
+
+    }
+
+    //can also use Graphics g
+    public void GameDraw(Graphics2D g2d){
+
+    }
+
+    //process keypressed events
+
+    public void gameKeyPressed(int keyCode){
+        switch (keyCode){
+            case KeyEvent.VK_UP:
+                break;
+            case KeyEvent.VK_DOWN:
+                break;
+            case KeyEvent.VK_RIGHT:
+                break;
+            case KeyEvent.VK_LEFT:
+                break;
+        }
+
+    }
+
 }
