75f842c4b86452b271565e7500209812d3b3233c
pratiksaxena <pratik2709@gmail.com>
2017-11-05 22:02:20 +0530

    Added game state and background

---
+++ src/TileMap/Background.java
@@ -0,0 +1,63 @@
+package TileMap;
+
+import Main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Background {
+
+    private BufferedImage image;
+
+    private int x;
+    private int y;
+    private double dx;
+    private double dy;
+    private int moveScale;
+
+    public Background(String s, int ms) {
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream(s));
+            moveScale = ms;
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = (x * moveScale)%GamePanel.WIDTH;
+        this.y = (y * moveScale)%GamePanel.HEIGHT;
+    }
+
+
+    public void setVector(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void update() {
+        x += dx;
+        y += dy;
+    }
+
+    public void draw(Graphics2D g) {
+        if (x < 0) {
+            g.drawImage(
+                    image,
+                    x + GamePanel.WIDTH,
+                    y,
+                    null
+            );
+        } else if (x > 0) {
+            g.drawImage(
+                    image,
+                    x - GamePanel.WIDTH,
+                    y,
+                    null
+            );
+        }
+    }
+}
