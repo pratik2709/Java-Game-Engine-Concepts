420d4564780d284978ffadc7c3d083b9212251e6
pratiksaxena <pratik2709@gmail.com>
2017-11-05 18:13:15 +0530

    Added menu state

---
+++ src/GameState/MenuState.java
@@ -0,0 +1,35 @@
+package GameState;
+
+import java.awt.*;
+
+public class MenuState extends GameState {
+
+    public MenuState(GameStateManager gsm){
+        this.gsm = gsm;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
+
+    @Override
+    public void keyReleased(int k) {
+
+    }
+
+    @Override
+    public void keyPressed(int k) {
+
+    }
+}
