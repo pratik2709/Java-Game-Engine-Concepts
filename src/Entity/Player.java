5259bbcb3c4f9a2bd034cd2fc8668478d5027421
Pratik <pratik2709@gmail.com>
2017-11-14 17:22:40 +0530

    completed update function

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -1,4 +1,5 @@
 package Entity;
+
 import TileMap.TileMap;
 
 import javax.imageio.ImageIO;
@@ -6,7 +7,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public class Player extends MapObject{
+public class Player extends MapObject {
 
     //GLIDING
     private boolean gliding;
@@ -26,7 +27,7 @@ public class Player extends MapObject{
     private static final int FIREBALL = 5;
     private static final int SCRATCHING = 6;
 
-    public Player(TileMap tm){
+    public Player(TileMap tm) {
         super(tm);
         //read from spritesheet
         width = 30;
@@ -54,27 +55,26 @@ public class Player extends MapObject{
         scratchRange = 40;
 
         //load sprites
-        try{
+        try {
             BufferedImage spriteSheet = ImageIO.read(
                     getClass().getResourceAsStream(
                             "/Sprites/Player/playersprites.gif"
                     )
             );
-            for(int i = 0; i < 7; i++){
+            for (int i = 0; i < 7; i++) {
                 BufferedImage[] bi = new BufferedImage[numFrames[i]];
-                for(int j = 0; j < numFrames[i]; j++){
-                    if(i != 6){
+                for (int j = 0; j < numFrames[i]; j++) {
+                    if (i != 6) {
                         bi[j] = spriteSheet.getSubimage(
-                                j*width,
-                                i*height,
+                                j * width,
+                                i * height,
                                 width,
                                 height
                         );
-                    }
-                    else{
+                    } else {
                         bi[j] = spriteSheet.getSubimage(
-                                j*width*2,
-                                i*height,
+                                j * width * 2,
+                                i * height,
                                 width,
                                 height
                         );
@@ -84,49 +84,49 @@ public class Player extends MapObject{
                 sprites.add(bi);
             }
 
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
     }
 
-    public int getHealth(){
+    public int getHealth() {
         return health;
     }
 
-    public int getMaxHealth(){
+    public int getMaxHealth() {
         return maxHealth;
     }
 
-    public int getFire(){
+    public int getFire() {
         return fire;
     }
 
-    public int getMaxFire(){
+    public int getMaxFire() {
         return maxFire;
     }
 
-    public void setFiring(){
+    public void setFiring() {
         firing = true;
     }
 
-    public void setScratching(){
+    public void setScratching() {
         scratching = true;
     }
 
-    public void setGliding(boolean b){
+    public void setGliding(boolean b) {
         gliding = b;
     }
 
-    public void update(){
+    public void update() {
         //??
         getNextPosition();
         checkTileMapCollision();
         setPosition(xtemp, ytemp);
 
         //set animation
-        if(scratching){
-            if(currentAction != SCRATCHING){
+        if (scratching) {
+            if (currentAction != SCRATCHING) {
                 currentAction = SCRATCHING;
                 animation.setFrame(sprites.get(SCRATCHING));
                 //??
@@ -134,17 +134,76 @@ public class Player extends MapObject{
                 //width of the sprite
                 width = 60;
             }
-        }
-        else if (firing){
-            if(currentAction != FIREBALL){
+        } else if (firing) {
+            if (currentAction != FIREBALL) {
                 currentAction = FIREBALL;
                 animation.setFrame(sprites.get(FIREBALL));
                 animation.setDelay(100);
                 width = 30;
             }
         }
-        else if(dy > 0){
+        //dy is direction ?
+        //falling animations
+        else if (dy > 0) {
+            if (currentAction != GLIDING) {
+                currentAction = GLIDING;
+                animation.setFrame(sprites.get(GLIDING));
+                //??
+                animation.setDelay(100);
+                //width of the sprite
+                width = 30;
+            } else if (currentAction != FALLING) {
+                currentAction = FALLING;
+                animation.setFrame(sprites.get(FALLING));
+                animation.setDelay(100);
+                width = 30;
+            }
 
         }
+        //jumping
+        //if -ve dy is added to y things would go up
+        else if (dy < 0) {
+            if (currentAction != JUMPING) {
+                currentAction = JUMPING;
+                animation.setFrame(sprites.get(JUMPING));
+                //since only 1 sprite??
+                animation.setDelay(-1);
+                width = 30;
+            }
+        }
+
+        else if (left || right) {
+            if (currentAction != WALKING) {
+                currentAction = WALKING;
+                animation.setFrame(sprites.get(WALKING));
+                animation.setDelay(40);
+                width = 30;
+            }
+        }
+        else{
+            //Idle frames
+            if (currentAction != IDLE) {
+                currentAction = IDLE;
+                animation.setFrame(sprites.get(IDLE));
+                animation.setDelay(400);
+                width = 30;
+            }
+        }
+        animation.update();
+
+        //???
+        //dont want player to move when he is attacking
+        if(currentAction != SCRATCHING && currentAction != FIREBALL){
+            if(right) facingRight = true;
+            if(left) facingRight = false;
+        }
+    }
+
+    public void draw(Graphics2D g){
+        //?? should be called in every map object why ?
+        setMapPostion();
+
+        //draw player
+//        if(fli)
     }
 }
