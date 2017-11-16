13564170861fa65c00419f74b6a0c4f8647c14e7
pratiksaxena <pratik2709@gmail.com>
2017-11-17 00:08:44 +0530

    getting the level class ready to draw the players

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -1,13 +1,17 @@
 package GameState;
 
+import Entity.*;
 import Main.GamePanel;
-import TileMap.TileMap;
+import TileMap.*;
 
 import java.awt.*;
 
 public class Level1State extends GameState {
 
     private TileMap tileMap;
+    private Background bg;
+
+    private Player player;
 
     public Level1State(GameStateManager gsm) {
         this.gsm = gsm;
@@ -20,19 +24,24 @@ public class Level1State extends GameState {
         tileMap.loadTiles("/Tilesets/grasstileset.gif");
         tileMap.loadMap("/Maps/level1-1.map");
         tileMap.setPosition(0,0);
+
+        bg = new Background("/Backgrounds/grassbg1.gif", (int) 0.1);
+
+        player = new Player(tileMap);
     }
 
     @Override
     public void update() {
+        player.update();
 
     }
 
     @Override
     public void draw(Graphics2D g) {
-        g.setColor(Color.WHITE);
-        g.fillRect(0,0, GamePanel.WIDTH, GamePanel.HEIGHT);
-
+        bg.draw(g);
         tileMap.draw(g);
+        //draw player
+        player.draw(g);
     }
 
     @Override
