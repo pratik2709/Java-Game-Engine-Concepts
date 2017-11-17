164764fd2c4308a4a8745e75e0385eda807f5d89
Pratik <pratik2709@gmail.com>
2017-11-17 15:09:45 +0530

    Added keypresses

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -5,6 +5,7 @@ import Main.GamePanel;
 import TileMap.*;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Level1State extends GameState {
 
@@ -47,11 +48,23 @@ public class Level1State extends GameState {
 
     @Override
     public void keyReleased(int k) {
-
+        if(k == KeyEvent.VK_LEFT) player.setLeft(false);
+        if(k == KeyEvent.VK_RIGHT) player.setRight(false);
+        if(k == KeyEvent.VK_UP) player.setUp(false);
+        if(k == KeyEvent.VK_DOWN) player.setDown(false);
+        if(k == KeyEvent.VK_W) player.setJumping(false);
+        if(k == KeyEvent.VK_E) player.setGliding(false);
     }
 
     @Override
     public void keyPressed(int k) {
-
+        if(k == KeyEvent.VK_LEFT) player.setLeft(true);
+        if(k == KeyEvent.VK_RIGHT) player.setRight(true);
+        if(k == KeyEvent.VK_UP) player.setUp(true);
+        if(k == KeyEvent.VK_DOWN) player.setDown(true);
+        if(k == KeyEvent.VK_W) player.setJumping(true);
+        if(k == KeyEvent.VK_E) player.setGliding(true);
+        if(k == KeyEvent.VK_R) player.setScratching();
+        if(k == KeyEvent.VK_F) player.setFiring();
     }
 }
