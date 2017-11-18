b0124fcbd95bb5d0af33184626fdbbaf5ea8f708
Pratik <pratik2709@gmail.com>
2017-11-18 14:49:48 +0530

    key events added

--- src/GameState/Level1State.java
+++ src/GameState/Level1State.java
@@ -35,6 +35,11 @@ public class Level1State extends GameState {
     @Override
     public void update() {
         player.update();
+        tileMap.setPosition(
+                GamePanel.WIDTH/2 - player.getx(),
+                GamePanel.HEIGHT/2 - player.gety()
+
+        );
 
     }
 
