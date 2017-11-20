c0a8eeed87c392b360d6ed4340ebb0eefe987340
Pratik <pratik2709@gmail.com>
2017-11-20 14:11:16 +0530

    added hit method

--- src/Entity/Enemy.java
+++ src/Entity/Enemy.java
@@ -10,11 +10,26 @@ public abstract class Enemy extends MapObject {
     protected int damage;
 
     protected boolean flinching;
-    protected boolean flinchTimer;
+    protected long flinchTimer;
 
     public Enemy(TileMap tm) {
         super(tm);
     }
 
-    
+    public boolean isDead(){
+        return dead;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public void hit(int damage){
+        if(dead || flinching) return;
+        health -= damage;
+        if(health < 0) health = 0;
+        if(health == 0) dead = true;
+        flinching = true;
+        flinchTimer = System.nanoTime();
+    }
 }
