8306cb5bc76918ce2b449fd547f821ab6338cd15
pratiksaxena <pratik2709@gmail.com>
2017-11-22 01:01:03 +0530

    Explosion class and methods

--- src/Entity/Explosion.java
+++ src/Entity/Explosion.java
@@ -1,6 +1,7 @@
 package Entity;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Explosion {
@@ -26,7 +27,7 @@ public class Explosion {
 
         try{
             BufferedImage spriteSheet = ImageIO.read(
-                    getClass().getResourceAsStream("/Sprites/Enemies/slugger.gif")
+                    getClass().getResourceAsStream("/Sprites/Enemies/explosion.gif")
             );
             sprites = new BufferedImage[3];
             for(int i = 0; i < sprites.length; i++){
@@ -42,5 +43,35 @@ public class Explosion {
             e.printStackTrace();
         }
 
+        animation = new Animation();
+        animation.setFrames(sprites);
+        animation.setDelay(70);
+
     }
+
+    public void update(){
+        animation.update();
+        if(animation.hasPlayedOnce()){
+            remove = true;
+        }
+    }
+
+    public boolean shouldRemove(){
+        return remove;
+    }
+
+    //??
+    public void setMapPosition(int x, int y){
+        xmap = x;
+        ymap = y;
+    }
+
+    public void draw(Graphics2D g){
+        g.drawImage(animation.getImage(),
+                x + xmap - width/2,
+                y + ymap - width/2,
+                null);
+    }
+
+
 }
