28247e0a0daedd82e54c0125c7af7cff567f98f5
pratiksaxena <pratik2709@gmail.com>
2017-11-25 15:57:07 +0530

    memory saving

--- src/GameState/GameStateManager.java
+++ src/GameState/GameStateManager.java
@@ -3,36 +3,57 @@ package GameState;
 import java.util.ArrayList;
 
 public class GameStateManager {
-    private ArrayList<GameState> gameStates;
+    private GameState[] gameStates;
     private int currentState;
+    public static final int NUMGAMESTATES = 2;
     public  static final int MENUSTATE = 0;
     public  static final int LEVEL1STATE = 1;
 
     public GameStateManager(){
-        gameStates = new ArrayList<GameState>();
+        gameStates = new GameState[NUMGAMESTATES];
         currentState = MENUSTATE;
-        gameStates.add(new MenuState(this));
-        gameStates.add(new Level1State(this));
+        loadState(currentState);
+    }
+
+    private void loadState(int state) {
+        if(state == MENUSTATE){
+            gameStates[state] = new MenuState(this);
+        }
+        else if(state == LEVEL1STATE){
+            gameStates[state] = new Level1State(this);
+        }
+    }
+
+    private void unLoadState(int state) {
+        gameStates[state] = null;
+
     }
 
     public void setState(int state){
+        unLoadState(currentState);
         currentState = state;
-        gameStates.get(currentState).init();
+        loadState(currentState);
+//        gameStates[currentState].init();
     }
 
     public void draw(java.awt.Graphics2D g){
-        gameStates.get(currentState).draw(g);
+        if(gameStates[currentState] != null){
+            gameStates[currentState].draw(g);
+        }
+
     }
 
     public void update(){
-        gameStates.get(currentState).update();
+        if(gameStates[currentState] != null){
+            gameStates[currentState].update();
+        }
     }
 
     public void keyPressed(int k){
-        gameStates.get(currentState).keyPressed(k);
+        gameStates[currentState].keyPressed(k);
     }
 
     public void keyReleased(int k){
-        gameStates.get(currentState).keyReleased(k);
+        gameStates[currentState].keyReleased(k);
     }
 }
